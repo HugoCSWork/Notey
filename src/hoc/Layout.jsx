aed3b85a2304e81e56d8b4c1c9851ec64adec4f2
hugo.tomas1 <hugo.tomas1@nhs.net>
2020-03-30 13:04:32 +0100

    Fix colors

--- src/hoc/Layout.jsx
+++ src/hoc/Layout.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 import Navbar from "../components/Navbar/Navbar";
 import SideDrawer from "../components/SideDrawer/SideDrawer";
+import Footer from "../components/Footer/Footer";
 // import SideDrawer from "../components/Navigation/SideDrawer/SideDrawer";
 // import Navbar from "../components/Navigation/Navbar/Navbar";
 
@@ -10,6 +11,7 @@ const MainWrapper = styled.main`
   width: 100%;
   min-height: calc(100vh - 6rem);
   margin-top: 6rem;
+  background-color: var(--color-main);
   display: flex;
   align-items: center;
   justify-content: center;
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -3,18 +3,11 @@
  */
 const theme = {
   colors: {
-    // main: "#2F2FA2",
-    // mainDarker: "#242582",
-    // textMain: "#f64C72",
-    footer: "#99738E",
-    // white: "#FFFFFF",
-    // shadow: "rgba(0,0,0,.2)",
-    // errorRed: "#ff5757"
-    main: "#2F2FA2",
-    mainDark: "#242582",
-    mainDarker: "#2F2FA2",
-    mainLight: "#2c3247",
-    mainLighter: "#00A8E8",
+    main: "#E5E7E6",
+    mainDark: "#A53860",
+    mainDarker: "#DA627D",
+    mainLight: "#A53860",
+    mainLighter: "#FFA5AB",
     textColor: "#333",
     whiteColor: "#FFFFFF",
     errorRed: "#ff5757",
